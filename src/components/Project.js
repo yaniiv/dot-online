f32bec3fe06ced232a5bc06be43d5672381c2317
yantonsoup <ygoldobin@gmail.com>
2019-03-07 20:51:51 -0800

    container dynamic styles

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,91 +1,30 @@
 import React from "react"
 // import { CloudinaryContext, Video, Transformation } from "cloudinary-react"
-// import * as Styles from "./Project.style"
+import * as styles from "./Project.style"
 import BrowserButton from "./BrowserButton"
 import { css } from "@emotion/core"
 
 const cloudinaryRootUrl = "https://res.cloudinary.com/dzprezr1g/video/upload/"
 
-const projectContainer = css`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`
 
-const project = css`
-  margin: auto;
-  font-size: 16px;
-  max-width: 640px;
-  max-height: 420px;
-`
-
-const browserFrame = css`
-  border: 8px solid black;
-  border-radius: 14px;
-  display: flex;
-  flex-direction: column;
-`
-
-const browserHeader = css`
-  border-bottom: 8px solid black;
-  height: 60px;
-  width: 100%;
-  display: flex;
-`
-
-const browserButtons = css`
-  flex: 1;
-  flex-direction: row;
-  display: flex;
-  padding: 0 16px;
-`
-
-const browserUrlBarContainer = css`
-  flex: 4;
-  padding: 8px 8px;
-`
-
-const browserUrlBar = css`
-  background: transparent;
-  border: 3px solid black;
-  border-radius: 6px;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`
-
-const browserUrlLink = css`
-  margin: auto 16px;
-  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen;
-  text-decoration: none;
-  font-size: 18px;
-  color: blue;
-  font-weight: 400;
-  letter-spacing: 0.3px;
-`
 
 const ProjectBlock = ({ name, link }) => (
-  <div css={projectContainer}>
-    <div css={project}>
-      <div>
-        <div>
+  <div css={styles.projectContainer}>
+    <div css={styles.project}>
           {/* <div style={{ position: 'relative', zIndex: '50'}}>
             <BrowserWindow />
           </div> */}
-          <div css={browserFrame}>
-            <div css={browserHeader}>
-              <div css={browserButtons}>
+          <div css={styles.browserFrame}>
+            <div css={styles.browserHeader}>
+              <div css={styles.browserButtons}>
                 <BrowserButton />
                 <BrowserButton />
                 <BrowserButton />
               </div>
-              <div css={browserUrlBarContainer}>
-                <div css={browserUrlBar}>
+              <div css={styles.browserUrlBarContainer}>
+                <div css={styles.browserUrlBar}>
                   <a
-                    css={browserUrlLink}
+                    css={styles.browserUrlLink}
                     href="https://aftertheussr.com"
                     target="_blank"
                     rel="noopener noreferrer"
@@ -95,6 +34,7 @@ const ProjectBlock = ({ name, link }) => (
                 </div>
               </div>
             </div>
+
             <video
               muted
               controls
@@ -102,8 +42,6 @@ const ProjectBlock = ({ name, link }) => (
               src={`${cloudinaryRootUrl}/${name}.mp4`}
             />
           </div>
-        </div>
-      </div>
     </div>
   </div>
 )
--- src/components/Project.style.js
+++ src/components/Project.style.js
@@ -1,121 +1,69 @@
 import { css } from "@emotion/core"
 
-const projectContainer = css`
+export const projectContainer = css`
   height: 100vh;
-  width: 100vw,
-  display: flex,
-  flexDirection: column,
-  justifyContent: center,
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `
 
-console.warn("projectContainer", projectContainer)
+export const project = css`
 
-export default {
-  projectContainer,
-}
+  margin: auto;
+  padding: 24px;
+  font-size: 16px;
+  max-width: 500px;
+  max-height: 468px;
 
-// const ProjectBlock = ({ name, link }) => (
-//   <div
-//     style={{
-//       height: "100vh",
-//       width: "100vw",
-//       display: "flex",
-//       flexDirection: "column",
-//       justifyContent: "center",
-//     }}
-//   >
-//     <div
-//       style={{
-//         margin: "auto",
-//         fontSize: "16px",
-//         maxWidth: "640px",
-//         maxHeight: "420px",
-//       }}
-//     >
-//       <div>
-//         <div style={{ margin: "0 auto", width: "100%", position: "relative" }}>
-//           {/* <div style={{ position: 'relative', zIndex: '50'}}>
-//             <BrowserWindow />
-//           </div> */}
-//           <div
-//             style={{
-//               border: "8px solid black",
-//               borderRadius: "14px",
-//               display: "flex",
-//               flexDirection: "column",
-//             }}
-//           >
-//             <div
-//               style={{
-//                 borderBottom: "8px solid black",
-//               }}
-//             >
-//               <div
-//                 className="spacer"
-//                 style={{
-//                   height: "60px",
-//                   width: "100%",
-//                   display: "flex",
-//                 }}
-//               >
-//                 <div
-//                   style={{
-//                     flex: "1",
-//                     flexDirection: "row",
-//                     display: "flex",
-//                     padding: "0 16px",
-//                   }}
-//                 >
-//                   <BrowserButton />
-//                   <BrowserButton />
-//                   <BrowserButton />
-//                 </div>
-//                 <div
-//                   style={{
-//                     flex: "4",
-//                     padding: "8px 8px",
-//                   }}
-//                 >
-//                   <div
-//                     style={{
-//                       background: "transparent",
-//                       border: "3px solid black",
-//                       borderRadius: "6px",
-//                       height: "100%",
-//                       display: "flex",
-//                       flexDirection: "column",
-//                       justifyContent: "center",
-//                     }}
-//                   >
-//                     <a
-//                       style={{
-//                         margin: "auto 16px",
-//                         fontFamily:
-//                           "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen",
-//                         textDecoration: "none",
-//                         fontSize: "18px",
-//                         color: "blue",
-//                         fontWeight: "400",
-//                         letterSpacing: "0.3px",
-//                       }}
-//                       href="https://aftertheussr.com"
-//                       target="_blank"
-//                     >
-//                       {link}
-//                     </a>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//             <video
-//               muted
-//               controls
-//               width="100%"
-//               src={`${cloudinaryRootUrl}/${name}.mp4`}
-//             />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// )
+  @media (min-width: 768px) {
+    max-width: 648px;
+    max-height: 468px;
+  }
+`
+
+export const browserFrame = css`
+  border: 8px solid black;
+  border-radius: 14px;
+  display: flex;
+  flex-direction: column;
+`
+
+export const browserHeader = css`
+  border-bottom: 8px solid black;
+  height: 60px;
+  width: 100%;
+  display: flex;
+`
+
+export const browserButtons = css`
+  flex: 1;
+  flex-direction: row;
+  display: flex;
+  padding: 0 16px;
+`
+
+export const browserUrlBarContainer = css`
+  flex: 4;
+  padding: 8px 8px;
+`
+
+export const browserUrlBar = css`
+  background: transparent;
+  border: 3px solid black;
+  border-radius: 6px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+
+export const browserUrlLink = css`
+  margin: auto 16px;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen;
+  text-decoration: none;
+  font-size: 18px;
+  color: blue;
+  font-weight: 400;
+  letter-spacing: 0.3px;
+`
