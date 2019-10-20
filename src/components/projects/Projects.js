c8285882f19c3c9eebc70f39ecfa057b5508f20b
yanton <ygoldobin@gmail.com>
2019-10-20 23:19:43 +0200

    shift data load to projects entry

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,32 +1,6 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
 
 import Project from "./Project"
-
-function normalizeProjectData(graphqlResponse) {
-  console.warn({ graphqlResponse })
-  const projectEdges = graphqlResponse.allPrismicProjects.edges
-  const projectData = projectEdges.map(project => {
-    const {
-      node: {
-        id,
-        data: { project_website, project_gif, project_description },
-      },
-    } = project
-
-    return {
-      id,
-      link: project_website.url,
-      gif: {
-        ...project_gif,
-      },
-      info: project_description.text,
-    }
-  })
-
-  console.warn("normalized:", { projectData })
-  return projectData
-}
 
 const Projects = ({ projects }) => (
   <>
@@ -36,38 +10,4 @@ const Projects = ({ projects }) => (
   </>
 )
 
-const ProjectsWithData = () => (
-  <StaticQuery
-    query={graphql`
-      query Projects {
-        allPrismicProjects {
-          edges {
-            node {
-              id
-              first_publication_date
-              data {
-                project_website {
-                  url
-                  target
-                }
-                project_gif {
-                  name
-                  url
-                  width
-                  height
-                }
-                project_description {
-                  html
-                  text
-                }
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={data => <Projects projects={normalizeProjectData(data)} />}
-  />
-)
-
-export default ProjectsWithData
+export default Projects
--- src/components/projects/ProjectsEntry.js
+++ src/components/projects/ProjectsEntry.js
@@ -1,15 +1,71 @@
-/* eslint-disable */
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
-// import { Link } from "gatsby"
 
 import Layout from "../Layout"
 import Projects from "./Projects"
 
+function normalizeProjectData(graphqlResponse) {
+  console.warn("allPrismicProjects:", { graphqlResponse })
+
+  const projectEdges = graphqlResponse.allPrismicProjects.edges
+  const projectData = projectEdges.map(project => {
+    const {
+      node: {
+        id,
+        data: { project_website, project_gif, project_description },
+      },
+    } = project
+
+    return {
+      id,
+      link: project_website.url,
+      gif: {
+        ...project_gif,
+      },
+      info: project_description.text,
+    }
+  })
+
+  console.warn("normalized allPrismicProjects:", { projectData })
+  return projectData
+}
+
 const ProjectsEntry = () => (
-  <Layout>
-    <Projects />
-  </Layout>
+  <StaticQuery
+    query={graphql`
+      query Projects {
+        allPrismicProjects {
+          edges {
+            node {
+              id
+              first_publication_date
+              data {
+                project_website {
+                  url
+                  target
+                }
+                project_gif {
+                  name
+                  url
+                  width
+                  height
+                }
+                project_description {
+                  html
+                  text
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Layout>
+        <Projects projects={normalizeProjectData(data)} />
+      </Layout>
+    )}
+  />
 )
 
 export default ProjectsEntry
