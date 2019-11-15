9583af695fa1e51342c1157cfef8a8f125db618c
yanton <ygoldobin@gmail.com>
2019-11-15 22:32:40 +0100

    remove sizes

--- src/sizes.js
+++
@@ -1,6 +0,0 @@
-export const HEADER_HEIGHT_MOBILE = `78px`
-export const HEADER_HEIGHT_DESKTOP = `100px`
-
-export const DUALITY = `calc(100vh - ${HEADER_HEIGHT_DESKTOP})`
-
-export const MAX_TEXT_WIDTH = "600px"
