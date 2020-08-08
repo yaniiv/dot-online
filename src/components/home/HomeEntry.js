acf1c86577becc85b3cc763b6df253917f136f8c
yaniv goldobin <ygoldobin@gmail.com>
2020-08-08 15:26:16 -0700

    ok

--- src/components/home/HomeEntry.js
+++ src/components/home/HomeEntry.js
@@ -2,12 +2,25 @@ import React from "react"
 // import React, { useState } from "react"
 // import { Waypoint } from "react-waypoint"
 import { css } from "@emotion/core"
+import SEO from "../Seo"
+import * as COLORS from "../../colors"
 
-import Layout from "../Layout"
 import Duality from "../threejs/Duality"
 // import Contact from "../contact/Contact"
 // import Projects from "../projects/Projects"
+const layoutStyles = backgroundColor => css`
+  height: 100%;
+  width: 100%;
+  /* background: ${backgroundColor}; */
+  margin: 0 auto;
 
+  a {
+    color: ${COLORS.YELLOW};
+  }
+  a:visited {
+    color: ${COLORS.ROSETTE};
+  }
+`
 // import SideNav from "../SideNav"
 // import Intro from "../about/Intro"
 // import TopNav from "../TopNav"
@@ -15,7 +28,6 @@ import Duality from "../threejs/Duality"
 // import About from "../about/About"
 // import MagicBorder from "./MagicBorder"
 
-import * as COLORS from "../../colors"
 // import Project from "../projects/Project"
 // import Projects from "../projects/Projects"
 const HomeEntry = () => {
@@ -23,195 +35,152 @@ const HomeEntry = () => {
   // const [isTransparent, setHeaderTransparent] = useState(false)
 
   return (
-    <Layout>
-      <Duality />
+    <div css={layoutStyles()}>
+      <SEO title="Home" lang="en" keywords={[`yaniv`, `goldobin`]} />
+      <main>
+        <Duality />
 
-      {/* <SideNav isTransparent={isTransparent} /> */}
-      {/* <Intro isTransparent={isTransparent} /> */}
-      {/* <TopNav isTransparent={isTransparent} /> */}
-      <div
-        css={css`
-          display: absolute;
-          background: ${COLORS.PURPLE};
-          color: ${COLORS.YELLOW};
-
-          position: absolute;
-          /* width: 300px; */
-
-          left: 12vw;
-          top: 86vh;
-          @media (min-width: 768px) {
-            top: 22vh;
-            left: 32vw;
-          }
-        `}
-      >
-        <span
+        {/* <SideNav isTransparent={isTransparent} /> */}
+        {/* <Intro isTransparent={isTransparent} /> */}
+        {/* <TopNav isTransparent={isTransparent} /> */}
+        <div
           css={css`
-            display: flex;
-            font-weight: bolder;
+            display: absolute;
+            background: ${COLORS.PURPLE};
+            color: ${COLORS.YELLOW};
+
+            position: absolute;
+            /* width: 300px; */
+
+            left: 12vw;
+            top: 86vh;
+            @media (min-width: 768px) {
+              top: 22vh;
+              left: 32vw;
+            }
           `}
         >
-          contact
-        </span>{" "}
-        <a href="mailto:hello@yaniv.online">hello@yaniv.online</a>
-      </div>
+          <span
+            css={css`
+              display: flex;
+              font-weight: bolder;
+            `}
+          >
+            contact
+          </span>{" "}
+          <a href="mailto:hello@yaniv.online">hello@yaniv.online</a>
+        </div>
 
-      <div
-        css={css`
-          display: absolute;
-          background: ${COLORS.TRANSPARENT};
-          color: ${COLORS.YELLOW};
-          /* right: 8vw; */
-          top: 6vh;
-          z-index: 20;
+        <div
+          css={css`
+            display: absolute;
+            background: ${COLORS.TRANSPARENT};
+            color: ${COLORS.YELLOW};
+            /* right: 8vw; */
+            top: 6vh;
+            z-index: 20;
 
-          left: 18vw;
-          position: absolute;
+            left: 18vw;
+            position: absolute;
 
-          @media (min-width: 768px) {
+            @media (min-width: 768px) {
+              left: 68vw;
+              top: 26vh;
+            }
+          `}
+        >
+          <div>
+            <div
+              css={css`
+                font-weight: 800;
+              `}
+            >
+              projects
+            </div>
+            <div>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="http://www.thiswebsiteisnotaboutyou.com/"
+              >
+                thiswebsiteisnotaboutyou.com
+              </a>
+            </div>
+            <div>
+              <a
+                rel="noreferrer"
+                target="_blank"
+                href="https://www.aftertheussr.com/"
+              >
+                aftertheussr.com
+              </a>
+            </div>
+            <div>
+              <a rel="noreferrer" target="_blank" href="https://sperry.work/">
+                sperry.work
+              </a>
+            </div>
+            <div>
+              <a
+                rel="noreferrer"
+                target="_blank"
+                href="https://www.yaniv.online/"
+              >
+                yaniv.online
+              </a>
+            </div>
+            <div>
+              <a
+                rel="noreferrer"
+                target="_blank"
+                href="https://github.com/dispersionjs/dispersion"
+              >
+                dispersion.js
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div
+          css={css`
+            display: absolute;
+            background: ${COLORS.TRANSPARENT};
+            color: ${COLORS.YELLOW};
+
+            position: absolute;
+
             left: 68vw;
-            top: 26vh;
-          }
-        `}
-      >
-        <div>
-          <div
-            css={css`
-              font-weight: 800;
-            `}
-          >
-            projects
-          </div>
-          <div>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="http://www.thiswebsiteisnotaboutyou.com/"
-            >
-              thiswebsiteisnotaboutyou.com
-            </a>
-          </div>
-          <div>
-            <a
-              rel="noreferrer"
-              target="_blank"
-              href="https://www.aftertheussr.com/"
-            >
-              aftertheussr.com
-            </a>
-          </div>
-          <div>
-            <a rel="noreferrer" target="_blank" href="https://sperry.work/">
-              sperry.work
-            </a>
-          </div>
-          <div>
-            <a
-              rel="noreferrer"
-              target="_blank"
-              href="https://www.yaniv.online/"
-            >
-              yaniv.online
-            </a>
-          </div>
-          <div>
-            <a
-              rel="noreferrer"
-              target="_blank"
-              href="https://github.com/dispersionjs/dispersion"
-            >
-              dispersion.js
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div
-        css={css`
-          display: absolute;
-          background: ${COLORS.TRANSPARENT};
-          color: ${COLORS.YELLOW};
-
-          position: absolute;
-
-          left: 68vw;
-          top: 56vh;
-          @media (min-width: 768px) {
-            left: 38vw;
             top: 56vh;
-          }
-        `}
-      >
-        <div>
-          <div
-            css={css`
-              font-weight: 800;
-            `}
-          >
-            etc.
-          </div>
+            @media (min-width: 768px) {
+              left: 38vw;
+              top: 56vh;
+            }
+          `}
+        >
           <div>
-            <a href="https://twitter.com/yantonsoup">twitter</a>
-          </div>
-          <div>
-            <a href="https://github.com/yantonsoup">github</a>{" "}
-          </div>
-          <div>
-            <a href="https://linkedin.com/in/ygoldobin">linkedin</a>
-          </div>
-          <div>
-            <a href="https://medium.com/@yaniv_g">medium</a>
+            <div
+              css={css`
+                font-weight: 800;
+              `}
+            >
+              etc.
+            </div>
+            <div>
+              <a href="https://twitter.com/yantonsoup">twitter</a>
+            </div>
+            <div>
+              <a href="https://github.com/yantonsoup">github</a>{" "}
+            </div>
+            <div>
+              <a href="https://linkedin.com/in/ygoldobin">linkedin</a>
+            </div>
+            <div>
+              <a href="https://medium.com/@yaniv_g">medium</a>
+            </div>
           </div>
         </div>
-      </div>
-
-      {/* <Projects /> */}
-
-      {/*         
-        <Waypoint
-          onEnter={() => {
-            setPauseDuality(false)
-            setHeaderTransparent(false)
-          }}
-          onLeave={() => {
-            setPauseDuality(true)
-            setHeaderTransparent(true)
-          }}
-        /> */}
-
-      {/* <Waypoint
-          onEnter={() => {
-            setPauseDuality(false)
-          }}
-          onLeave={() => {}}
-        />
-
-
-        <div>
-      <About />
-            <MagicBorder />
-
-      </div>
-      <Waypoint
-        onEnter={() => {
-          setPauseDuality(true)
-        }}
-        onLeave={() => {}}
-      />
-
-
-      <Contact /> */}
-      {/* <div
-        css={css`
-          position: absolute;
-          top: 30vh;
-          left: 30vh;
-        `}
-      >
-        <MagicBorder />
-      </div> */}
-    </Layout>
+      </main>
+    </div>
   )
 }
 
